df2292a9292608b8556a6a66b55fff5e3ccd33e9
Palo Kisa <palo.kisa@gmail.com>
2021-01-10 00:10:52 +0100

    build: Use libjson downloaded/built in build time

--- FindJsonCpp.cmake
+++ FindJsonCpp.cmake
@@ -1,13 +1,8 @@
-find_package(PkgConfig)
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_JSONCPP jsoncpp QUIET)
-endif()
-
 find_path(JSONCPP_INCLUDE_DIRS json/json.h
-                               PATHS ${PC_JSONCPP_INCLUDEDIR}
-                               PATH_SUFFIXES jsoncpp)
+  PATHS ${CMAKE_PREFIX_PATH}
+  PATH_SUFFIXES jsoncpp)
 find_library(JSONCPP_LIBRARIES jsoncpp
-                               PATHS ${PC_JSONCPP_LIBDIR})
+  PATHS ${CMAKE_PREFIX_PATH})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(JsonCpp REQUIRED_VARS JSONCPP_LIBRARIES JSONCPP_INCLUDE_DIRS)
