8cc70fdd29589e457467124e0856ec10fad31d72
Alwin Esch <alwin.esch@web.de>
2021-09-20 05:43:44 +0200

    API change related code update

--- src/Data.cpp
+++ src/Data.cpp
@@ -389,6 +389,7 @@ PVR_ERROR Data::GetCapabilities(kodi::addon::PVRCapabilities& capabilities)
   capabilities.SetSupportsTV(true);
   capabilities.SetSupportsRadio(true);
   capabilities.SetSupportsRecordings(true);
+  capabilities.SetSupportsRecordingsDelete(true);
   capabilities.SetSupportsRecordingsUndelete(false);
   capabilities.SetSupportsTimers(true);
   capabilities.SetSupportsChannelGroups(true);
